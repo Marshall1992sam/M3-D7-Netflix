f2004bb97700d7f1876145b41dda7e4065c13966
Marshall1992sam <135346579+Marshall1992sam@users.noreply.github.com>
2023-09-02 16:39:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var carousel1 = document.getElementById("myCarousel1");
+  var myCarousel1 = new bootstrap.Carousel(carousel1, {
+    interval: false,
+  });
+  var carousel2 = document.getElementById("myCarousel2");
+  var myCarousel2 = new bootstrap.Carousel(carousel2, {
+    interval: false,
+  });
+  var carousel3 = document.getElementById("myCarousel3");
+  var myCarousel3 = new bootstrap.Carousel(carousel3, {
+    interval: false,
+  });
+});
